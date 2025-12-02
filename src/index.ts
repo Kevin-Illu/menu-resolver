e831a6e7e4d0e347f7231297cb5a8e2dc795095d
kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2025-12-01 18:18:38 -0600

    docs(types): se agregaron los tipos para los usuarios de la libreria

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
 import { randomUUID } from "node:crypto";
-import { Menu, Node, ResolverAPI } from "./types";
-export * from "./types";
+
+export type Node = {
+  id: string;
+  label: string;
+  resolve?: () => void | any;
+  parentKey: string | null;
+}
+
+export type ResolverAPI = {
+  goBack: () => void,
+}
+
+export type Menu = Prettify<MenuParent | MenuActionSimple | MenuActionCustom>;
+
+export type MenuParent = {
+  label: string;
+  children: Menu[];
+  resolve?: undefined;
+};
+
+export type MenuActionSimple = {
+  label: string;
+  resolve: string;
+  children?: Menu[] | undefined;
+};
+
+export type MenuActionCustom = {
+  label: string;
+  resolve: (rsApi: ResolverAPI) => any;
+  children?: Menu[] | undefined;
+};
+
+// just utils
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
 
 export default class TreeMenuResolver {
   private flatMapMenu: Map<
